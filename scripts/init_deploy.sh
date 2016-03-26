b45c2a2f1b262398ff8b91c7ce747d35284c06eb
Gor Martsen <Gor.martsen@gmail.com>
2016-03-26 04:33:55 -0500

    Relate to backdrop/backdrop-issues#1297  backdrop/backdrop#1310
    Apply patch to make sure that new PR is using DOCROOT for settings.php
    and files directory

--- scripts/init_deploy.sh
+++ scripts/init_deploy.sh
@@ -4,12 +4,19 @@
 #set site path
 SITEPATH="$HOME/domains/$DOMAIN"
 
-# Go to domain dir
+# Go to domain dir.
 cd $SITEPATH
 
 #link backdrop files
 ln -s $GITLC_DEPLOY_DIR/* ./
 ln -s $GITLC_DEPLOY_DIR/.htaccess ./
+
+# Unlink settings.php and copy instead.
+rm -f settings.php
+cp $GITLC_DEPLOY_DIR settings.php
+
+# Apply patch from PR #1310.
+cat /home/qa/tools/patches/1310.patch|patch -p 1
 
 #install backdrop
 php $GITLC_DEPLOY_DIR/core/scripts/install.sh --root=$SITEPATH --db-url=mysql://$DATABASE_USER:$DATABASE_PASSWORD@localhost/$DATABASE_NAME --account-mail=$ACCOUNT_MAIL --account-name=$SITE_USER --account-pass="$SITE_PASSWORD" --site-mail=$SITE_MAIL --site-name="$SITE_NAME"
