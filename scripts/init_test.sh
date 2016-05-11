c121ca01cec469037b7534e2cec7e042c7ca34aa
Gor Martsen <Gor.martsen@gmail.com>
2016-05-10 21:47:45 -0400

    Update init test script

--- scripts/init_test.sh
+++ scripts/init_test.sh
@@ -7,6 +7,19 @@ SITEPATH="$HOME/www"
 # Go to domain dir.
 cd $SITEPATH
 
+#link backdrop files
+ln -s $GITLC_DEPLOY_DIR/* ./
+ln -s $GITLC_DEPLOY_DIR/.htaccess ./
+
+# Unlink settings.php and copy instead.
+rm -f settings.php
+cp $GITLC_DEPLOY_DIR/settings.php ./
+
+# Unlink files and copy instead.
+rm -f files
+cp -r $GITLC_DEPLOY_DIR/files ./
+
+
 #install backdrop
 
 php $SITEPATH/core/scripts/install.sh  --db-url=mysql://test:localhost/test
