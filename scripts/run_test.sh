55ef11345e51b482c3d3e5dec34ed97553efc143
Geoff St. Pierre <serundeputy@gmail.com>
2018-07-20 14:37:57 -0400

    Adding phpcs code checking to tests.

--- scripts/run_test.sh
+++ scripts/run_test.sh
@@ -7,4 +7,9 @@ SITEPATH="$HOME/www"
 echo "Full site path: $SITEPATH"
 cd $SITEPATH
 
+# Install and run backdrop/coder
+composer require backdrop/coder
+./vendor/bin/phpcs --standard=./vendor/backdrop/coder/coder_sniffer/Backdrop core
+
+# Run the Backdrop test suites.
 php core/scripts/run-tests.sh --url http://localhost --verbose --cache --force --class UserAccountLinksUnitTests
